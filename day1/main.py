cb459ef87941083f22a0b36bfebcf09ab6152af8
Kayti <kaytipotgieter@gmail.com>
2023-09-15 13:52:02 -0600

    added files

---
+++ day1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!\n")
+home = input("where did you grow up? ")
+pet = input("what is the name of your first pet? ")
+print("Your band name should be: The " + home + " " + pet + "s")
