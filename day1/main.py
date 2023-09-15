be6eb595847fcef0d3bfa2814fd562e931f7875f
Kayti <kaytipotgieter@gmail.com>
2023-09-15 14:01:43 -0600

    added notes

--- day1/main.py
+++ day1/main.py
@@ -2,8 +2,11 @@
 # starting off lite and easy with a band name generator that
 # takes in ueser input and outputs a suggested band name.
 
-
+# Create a greeting for the program.
 print("Welcome to the band name generator!\n")
-home = input("where did you grow up? ")
-pet = input("what is the name of your first pet? ")
+# Ask the user where they grew up
+home = input("where did you grow up?\n")
+# Ask the user wwhat the name of their first pet.
+pet = input("what is the name of your first pet?\n")
+# Combine home and pet and show the band name
 print("Your band name should be: The " + home + " " + pet + "s")
