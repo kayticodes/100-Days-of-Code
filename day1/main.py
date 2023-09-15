67300c7c43fc3403e496a528445542c4a74a46e3
Kayti <kaytipotgieter@gmail.com>
2023-09-15 13:55:22 -0600

    added notes

--- day1/main.py
+++ day1/main.py
@@ -1,3 +1,8 @@
+# Day 1 of the 100 days of coding challenge
+# starting off lite and easy with a band name generator that
+# takes in ueser input and outputs a suggested band name.
+
+
 print("Welcome to the band name generator!\n")
 home = input("where did you grow up? ")
 pet = input("what is the name of your first pet? ")
