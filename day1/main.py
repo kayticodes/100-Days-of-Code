b4aea44fd270a63245fbbefed17f8db29a18a6e4
Kayti <kaytipotgieter@gmail.com>
2023-09-15 17:45:33 -0600

    added day2 folder

--- day1/main.py
+++ day1/main.py
@@ -3,10 +3,10 @@
 # takes in ueser input and outputs a suggested band name.
 
 # Create a greeting for the program.
-print("Welcome to the band name generator!\n")
+print("Welcome to the band name generator!")
 # Ask the user where they grew up
 home = input("where did you grow up?\n")
 # Ask the user wwhat the name of their first pet.
 pet = input("what is the name of your first pet?\n")
 # Combine home and pet and show the band name
-print("Your band name should be: The " + home + " " + pet + "s")
+print("Your band name could be: The " + home + " " + pet + "s")
