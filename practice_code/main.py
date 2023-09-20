cd58846b0af5267bb127aa344ea6e7d7ad56659c
Kayti <kaytipotgieter@gmail.com>
2023-09-19 20:53:56 -0600

    added practice file and code

---
+++ practice_code/main.py
@@ -0,0 +1,22 @@
+# A file for practice challenges to go
+
+# Find odd or even challenge
+num = input("please enter an integer: ")
+if int(num) % 2 == 0:
+    print(f"{num} is an even integer.")
+else:
+    print(f"{num} is an odd integer.")
+
+# Practice Challenge
+# yrs = 90 - int(input("What is your current age? "))
+# print(yrs)
+# days = yrs * 365
+# weeks = yrs * 52
+# months = yrs * 12
+# print(f"you have {days} days, {weeks} weeks, and {months} months left. ")
+
+# Practice with strings
+# num = input("please provide a 2 digit number: ")
+# first = int(num[0])
+# second = int(num[1])
+# print(first + second)
